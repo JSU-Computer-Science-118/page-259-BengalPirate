59f93712f8eb882d37cbec74fe79fdbd71fcdc6b
BengalPirate <114821633+BengalPirate@users.noreply.github.com>
2022-11-11 17:12:46 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -12,24 +12,24 @@ int main()
   double a;
   double b;
   double c;
-  double X_1;
-  double X_2;
+  double X1;
+  double X2;
   double discriminant;
 
   cin >> a >> b >> c;
 
   discriminant = pow(b, 2) - (4 * a * c);
-  X_1 = (-b + sqrt(discriminant))/(2*a);
-  X_2 = (-b - sqrt(discriminant))/(2*a);
+  X1 = (-b + sqrt(discriminant))/(2*a);
+  X2 = (-b - sqrt(discriminant))/(2*a);
 
   if (discriminant == 0){
-    cout << "The single root is "<< X_1 << endl;
+    cout << "The single root is "<< X1 << endl;
   }
 
   else if(discriminant > 0){
     cout << "Two real roots\n";
-    cout <<"- First root " << X_1 << "\n";
-    cout <<"- Second root " << X_2 << "\n";
+    cout <<"- First root " << X1 << "\n";
+    cout <<"- Second root " << X2 << "\n";
   }
 
   else if (discriminant < 0) {
